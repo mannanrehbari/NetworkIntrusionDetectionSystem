2960b09b7643316a1f3f578ba714d30531e3150d
Mannan <abdul.mannan@mavs.uta.edu>
2024-04-22 22:01:20 -0400

    Flink App Skeleton
    Flink App Skeleton

---
+++ IDSFlinkApp/src/main/java/com/eel/group9/IDS/IDSApp.java
@@ -0,0 +1,13 @@
+package com.eel.group9.IDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IDSApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IDSApp.class, args);
+	}
+
+}
